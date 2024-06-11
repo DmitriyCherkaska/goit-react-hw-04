6de2d33e3102a1066c5f7ae18c5344f80fa548bd
DmitriyCherkaska <151064884+DmitriyCherkaska@users.noreply.github.com>
2024-06-11 17:24:39 +0300

    fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-// import { Toaster } from "react-hot-toast";
+import { toast } from "react-hot-toast";
 import { useState, useEffect } from "react";
 import SearchBar from "./SearchBar/SearchBar";
 import ImageGallery from "./ImageGallery/ImageGallery";
@@ -27,28 +27,32 @@ const App = () => {
       const response = await fetch(url, {
         method: "GET",
         headers: {
-          API_URL,
+          Authorization: `Client-ID ${API_URL}`,
         },
       });
+      console.log(response);
       if (response.ok) {
         const data = await response.json();
-        setImages((prevImages) => [...prevImages, ...data.results]);
+        console.log(data);
+        if (Array.isArray(data.results)) {
+          setImages((prevImages) => [...prevImages, ...data.results]);
+        }
       } else {
+        console.error("Error: data.results is not an array");
         throw new Error(`Error ${response.status}: ${response.statusText}`);
       }
     } catch (error) {
-      console.error("Error occurred:", error);
       setError(error);
+      toast.error(`Error: ${error.message}`);
     } finally {
       setLoading(false);
     }
   };
-
   const handleSubmit = (searchTerm) => {
-    console.log("Search term:", searchTerm);
     setImages([]);
     setPage(1);
     setSearchTerm(searchTerm);
+    fetchImages(searchTerm, 1);
   };
 
   const handleLoadMore = () => {
@@ -66,7 +70,6 @@ const App = () => {
   };
 
   useEffect(() => {
-    console.log("Use effect is called");
     if (searchTerm !== "") {
       fetchImages(searchTerm, page);
     }
@@ -75,9 +78,12 @@ const App = () => {
   return (
     <div>
       <SearchBar onSubmit={handleSubmit} />
-      {loading && <Loader />}
-      {error && <ErrorMessage />}
-      <ImageGallery images={images} onImageClick={handleImageClick} />
+      {loading ? (
+        <Loader />
+      ) : (
+        <ImageGallery images={images} onImageClick={handleImageClick} />
+      )}
+      {error && <ErrorMessage message={error.message} />}
       {!loading && images.length > 0 && (
         <LoadMoreBtn onLoadMore={handleLoadMore} />
       )}
--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,17 +1,17 @@
 import { Audio } from "react-loader-spinner";
 
-<Audio
-  height="80"
-  width="80"
-  radius="9"
-  color="green"
-  ariaLabel="loading"
-  wrapperStyle
-  wrapperClass
-/>;
-
 const Loader = () => {
-  return <div>Loader</div>;
+  return (
+    <Audio
+      height="80"
+      width="80"
+      radius="9"
+      color="green"
+      ariaLabel="loading"
+      wrapperStyle
+      wrapperClass
+    />
+  );
 };
 
 export default Loader;

f5754b0f210c9149b2b942ca86b37f6485602072
DmitriyCherkaska <151064884+DmitriyCherkaska@users.noreply.github.com>
2024-06-13 15:38:44 +0300

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import toast from "react-hot-toast";
 import axios from "axios";
 import { useState, useEffect } from "react";
-import SearchBar from "./SearchBar/SearchBar";
-import ImageGallery from "./ImageGallery/ImageGallery";
-import Loader from "./Loader/Loader";
-import ErrorMessage from "./ErrorMessage/ErrorMessage";
-import LoadMoreBtn from "./LoadMoreBtn/LoadMoreBtn";
-import ImageModal from "./ImageModal/ImageModal";
+import SearchBar from "./components/SearchBar";
+import ImageGallery from "./components/ImageGallery";
+import Loader from "./components/Loader";
+import ErrorMessage from "./components/ErrorMessage";
+import LoadMoreBtn from "./components/LoadMoreBtn";
+import ImageModal from "./components/ImageModal";
 import "./App.css";
 
 const App = () => {
